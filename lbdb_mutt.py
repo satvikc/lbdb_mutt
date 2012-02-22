b6cfde464dd0499fea74ca546ca33c69dce2003e
satvikc <mystic.satvik@gmail.com>
2012-02-22 21:30:23 +0530

    Some file read errors corrected

--- lbdb_mutt.py
+++ lbdb_mutt.py
@@ -1,8 +1,11 @@
-import os,sys,subprocess,time,datetime,shutil
+#!/usr/bin/python
+import os,sys,subprocess,time,datetime,shutil,re
 MAILDIR=os.path.expanduser("~/Mail")
 LBDB=os.path.expanduser("~/.lbdb")
 FIRST_TIME=True
 DAY=1
+def is_email(a):
+    return (re.match('[a-zA-Z0-9]+@[a-zA-Z0-9]+(\.[a-z]+)+',a) is not None)
 
 def lbdb(x):
     if not os.path.isdir(LBDB):
@@ -19,14 +22,26 @@ def lbdb(x):
     shutil.move(LBDB+'/'+'m_inmail.list',LBDB+'/'+'m_inmail.list.temp')
     subprocess.getoutput('sort -u -d -f -i -k 1,1 '+ LBDB +'/m_inmail.list.temp > ' + LBDB+'/m_inmail.list')
     fp=open(LBDB+'/m_inmail.list.temp',"r")
-    lines=fp.readlines()
+    lines = []
+    line = ' '
+    while line :
+        try :
+            line = fp.readline() 
+            lines.append(line)
+        except :
+            pass 
+    #lines=fp.readlines()
     fp.close()
     tdict={}
     for l in lines:
-        tdict[l[:l.index('\t')]]=l
+        try:
+            tdict[l[:l.index('\t')]]=l
+        except :
+            pass
     lines2=[]
     for key in tdict:
-        lines2.append(tdict[key])
+        if is_email(key):
+            lines2.append(tdict[key])
     lines2.sort()
     fp=open(LBDB+'/m_inmail.list',"w")
     fp.writelines(lines2)
